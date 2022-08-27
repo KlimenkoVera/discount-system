26f459f82345908b8836d8ea138334f88578c20e
KlimenkoVera <klimenkovera1991@gmail.com>
2022-08-27 19:34:23 +0300

    Расчет суммы с учетом скидки

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,7 +1,26 @@
 package ru.netology
 
 fun main () {
+    val amountOfBuy = 10_050
+    val standardDiscount = 100
+    val discountPercent = 5
+    val discountMusicLover = 1
+    val priceMin = 1_000
+    val priceMax = 10_000
+    val isToMusicLover = true
 
+    var sumBuy = 0
 
+    if (amountOfBuy > priceMax) {
+        sumBuy = amountOfBuy - amountOfBuy * discountPercent / 100
+    } else if (amountOfBuy < priceMin) {
+        sumBuy = amountOfBuy
+    } else {
+        sumBuy = amountOfBuy - standardDiscount
+    }
+
+    val result = if (isToMusicLover) sumBuy - sumBuy * discountMusicLover / 100  else sumBuy
+
+    println("Сумма ваших покупок с учетом скидки составила: $result")
 
 }
